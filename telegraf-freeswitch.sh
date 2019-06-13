b29c7976e40d7e5550aff7d3113bf3f9ddf05380
Akhil Jalagam <akhiljalagams@gmail.com>
2019-06-13 11:59:50 +0530

    Update telegraf-freeswitch.sh

--- telegraf-freeswitch.sh
+++ telegraf-freeswitch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Author: Akhil Jalagam
+
 
 num_calls=`fs_cli -x "show channels count" | grep . | awk '{print $1}'`
 num_channels=`fs_cli -x "show status" | grep peak | grep -v 'per Sec' | awk '{print $1}'`
