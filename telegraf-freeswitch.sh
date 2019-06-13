96c5bb3635bd02bfd5f083c4e36a157c1fe6c58f
Akhil Jalagam <akhiljalagams@gmail.com>
2019-06-13 11:49:05 +0530

    Update telegraf-freeswitch.sh

--- telegraf-freeswitch.sh
+++ telegraf-freeswitch.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-"""
+'''
 FreeSWITCH Metrics
 This script collects the following FreeSWITCH metrics:
     - total of active channels
     - total of calls
     - Current CPS
-"""
+
+Usage:
+	./telegraf-freeswitch.sh
+	{"active_channels": 4, "active_calls": 12, "cps": 1}    
+'''
 
 num_calls=`fs_cli -x "show channels count" | grep . | awk '{print $1}'`
 num_channels=`fs_cli -x "show status" | grep peak | grep -v 'per Sec' | awk '{print $1}'`
